07b65dc249199d381bd4def29324fe29596bf69d
Sandra.B <105020067+Sandra@users.noreply.github.com>
2023-10-19 13:41:55 +0200

    rework

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+//function til oppgaven
 function convertLength() {
     const myInput = document.querySelector("#myInputID").value;
     const toValue = document.querySelector("#toIDValue").value;
     const fromValue = document.querySelector("#fromIDValue").value;
     const displayValue = document.querySelector("#displayValueID");
     
+    //Alert til nor man bruke det samme valge
     let result;
      if (toValue === fromValue) {
          alert("Same value selected");
          return; //stop current function
      }
+     // Starter med CM først er mer logisk får meg å starte med det 
     if (fromValue === "cm" && toValue === "m"){
 		result = myInput / 100;
 	}else if(fromValue === "cm" && toValue ==="km"){
@@ -17,6 +20,7 @@ function convertLength() {
         result = myInput * 10;
     }else if(fromValue === "cm" && toValue === "mil"){
         result = myInput / 1000000;
+        // starter på M her
     }else if(fromValue === "m" && toValue === "cm"){
         result = myInput / 100000;
     }else if(fromValue === "m" && toValue === "mm"){
@@ -25,6 +29,7 @@ function convertLength() {
         result = myInput / 10000;
     }else if(fromValue === "m" && toValue === "km"){
         result = myInput * 100;
+        // starter på MM her
     }else if(fromValue === "mm" && toValue === "m"){
         result = myInput  / 1000;
     }else if(fromValue === "mm" && toValue === "cm"){
@@ -33,6 +38,7 @@ function convertLength() {
         result = myInput / 10000000;
     }else if(fromValue === "mm" && toValue === "km"){
         result = myInput / 1000000;
+        //Starter på km her
     }else if(fromValue === "km" && toValue === "cm"){
         result = myInput / 1000;
     }else if(fromValue === "km" && toValue === "m"){
@@ -41,6 +47,7 @@ function convertLength() {
         result = myInput * 1000000;
     }else if(fromValue === "km" && toValue === "mil"){
         result = myInput / 10;
+        //starter på mil her
     }else if(fromValue === "mil" && toValue === "m"){
         result = myInput / 10000;
     }else if(fromValue === "mil" && toValue === "cm"){
@@ -52,4 +59,5 @@ function convertLength() {
     }                   
     displayValue.textContent = result.toFixed(3);
 }
+
 
